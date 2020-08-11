3d785079fc0c8704fccaf54646c4c3c5aeb4474e
Ivan Morales <ivan@perfilan.com>
2020-08-11 16:39:28 -0500

    Fetch para obtener lista de Peliculas

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Pelicula from './components/Pelicula';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      peliculas: [],
+    };
+
+    // this.handleInputImg = this.handleInputImg.bind(this);
+  }
 
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
+
+          <div class="container">
+            <div class="row">
+              {
+                this.state.peliculas.map((itemPelicula, index) =>
+                  <Pelicula pelicula={itemPelicula} />
+                )
+              }
+            </div>
+
+          </div>
+
         </header>
       </div>
     );
   }
 
+
+  componentDidMount() {
+
+    console.log('env', process.env);
+
+    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}&language=es-MX&page=1`;
+    fetch(url)
+      .then(request => request.json())
+      .then(data => {
+        this.setState({ peliculas: data.results })
+      });
+  }
 
 }
 
